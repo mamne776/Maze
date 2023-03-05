da94b29884288a414825bcec8bfd1010eaa968b2
mamne776 <mamne776@gmail.com>
2023-03-05 18:28:12 +0200

    MazeGenerator work

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -34,7 +34,7 @@ public class MazeGenerator : MonoBehaviour
     public Cell[,] CreateMaze(int height, int width)
     {
         //Cell[,] maze = new Cell[height, width];
-
+        //Debug.Log("Creating Maze!");
         maze = CreateMazeWithOnlyWalls(height, width);
 
         List<Cell> listOfUncheckedCells = new List<Cell>();
@@ -44,12 +44,63 @@ public class MazeGenerator : MonoBehaviour
         int randW = Random.Range(0, width);
         Cell cell = maze[randH, randW];
 
+        Debug.Log("cell pos:" + cell.heightPos + ", " + cell.widthPos);
+
         //mark this cell as checked
         cell.hasBeenChecked = true;
 
         //get a list of its neighbours
         SetNeighbours(cell);
 
+        //For each neighbor, starting with a randomly selected neighbor:
+        //If that neighbor hasn't been visited,
+        //remove the wall between this cell and that neighbor,
+        //and then recurse with that neighbor as the current cell.
+
+        //for each neighbour, starting with randomly selected neighbour
+        int randomForFirst = Random.Range(0, 4);
+
+        List<int> orderNumbers = new List<int>() { 0, 1, 2, 3 };
+        //pick first neighbour
+        int first = orderNumbers[randomForFirst];
+        //remove the orderNumber from the list
+        orderNumbers.RemoveAt(randomForFirst);
+
+        if (!cell.neighbours[first].hasBeenChecked && cell.neighbours[first] != null)
+        {
+            //remove the wall between cell and neighbour
+            RemoveWallBetween(cell, cell.neighbours[first]);
+        }
+
+        //pick second
+        int randomForSecond = Random.Range(0, 3);
+        int second = orderNumbers[randomForSecond];
+        orderNumbers.RemoveAt(randomForSecond);
+        if (!cell.neighbours[second].hasBeenChecked && cell.neighbours[second] != null)
+        {
+            //remove the wall between cell and neighbour
+            RemoveWallBetween(cell, cell.neighbours[second]);
+        }
+
+        //pick third
+        int randomForThird = Random.Range(0, 2);
+        int third = orderNumbers[randomForThird];
+        orderNumbers.RemoveAt(randomForThird);
+        if (!cell.neighbours[third].hasBeenChecked && cell.neighbours[third] != null)
+        {
+            //remove the wall between cell and neighbour
+            RemoveWallBetween(cell, cell.neighbours[third]);
+        }
+
+        //pick last
+        int fourth = orderNumbers[0];
+        if (!cell.neighbours[fourth].hasBeenChecked && cell.neighbours[fourth] != null)
+        {
+            //remove the wall between cell and neighbour
+            RemoveWallBetween(cell, cell.neighbours[fourth]);
+        }
+
+        /*
         //add the neighbours to the list of Cells we still need to check
         for (int i = 0; i < 4; i++)
         {
@@ -59,75 +110,75 @@ public class MazeGenerator : MonoBehaviour
                 listOfUncheckedCells.Add(cell.neighbours[i]);
             }
         }
+        */
 
-        //For each neighbor, starting with a randomly selected neighbor:
-        //If that neighbor hasn't been visited,
-        //remove the wall between this cell and that neighbor,
-        //and then recurse with that neighbor as the current cell.
-
-        //select random neighbour
-        int randNeighbour = Random.Range(0, 4);
-        while (cell.neighbours[randNeighbour] == null)
-        {
-            randNeighbour = Random.Range(0, 4);
-        }
-
+        /*
         //dangerzone
         while (listOfUncheckedCells.Count > 0)
         {
+            //select random neighbour
+            int randNeighbour = Random.Range(0, 4);
 
-        }
+
+            while (cell.neighbours[randNeighbour] == null)
+            {
+                randNeighbour = Random.Range(0, 4);
 
 
-        //remove the wall
-        switch (randNeighbour)
-        {
-            case 0:
-                if (!cell.neighbours[0].hasBeenChecked)
+
+
+
+                //remove the wall
+                switch (randNeighbour)
                 {
-                    //remove the wall on this cells left
-                    cell.walls[0] = false;
-                    cell.surroundingWalls--;
-                    //remove the wall from the neighbours right
-                    cell.neighbours[0].walls[2] = false;
-                    cell.neighbours[0].surroundingWalls--;
-                    //remove the cell from the list of cells that still need to be checked
-                    listOfUncheckedCells.Remove(cell);
+                    case 0:
+                        if (!cell.neighbours[0].hasBeenChecked)
+                        {
+                            //remove the wall on this cells left
+                            cell.walls[0] = false;
+                            cell.surroundingWalls--;
+                            //remove the wall from the neighbours right
+                            cell.neighbours[0].walls[2] = false;
+                            cell.neighbours[0].surroundingWalls--;
+                            //remove the cell from the list of cells that still need to be checked
+                            listOfUncheckedCells.Remove(cell);
+                        }
+                        break;
+
+                    case 1:
+                        if (!cell.neighbours[1].hasBeenChecked)
+                        {
+                            cell.walls[1] = false;
+                            cell.surroundingWalls--;
+
+                            cell.neighbours[1].walls[3] = false;
+                            cell.neighbours[1].surroundingWalls--;
+                        }
+                        break;
+
+                    case 2:
+                        if (!cell.neighbours[2].hasBeenChecked)
+                        {
+                            cell.walls[2] = false;
+                            cell.surroundingWalls--;
+
+                            cell.neighbours[2].walls[0] = false;
+                            cell.neighbours[2].surroundingWalls--;
+                        }
+                        break;
+
+                    case 3:
+                        if (!cell.neighbours[3].hasBeenChecked)
+                        {
+                            cell.walls[3] = false;
+                            cell.surroundingWalls--;
+
+                            cell.neighbours[3].walls[1] = false;
+                            cell.neighbours[3].surroundingWalls--;
+                        }
+                        break;
                 }
-                break;
-
-            case 1:
-                if (!cell.neighbours[1].hasBeenChecked)
-                {
-                    cell.walls[1] = false;
-                    cell.surroundingWalls--;
-
-                    cell.neighbours[1].walls[3] = false;
-                    cell.neighbours[1].surroundingWalls--;
-                }
-                break;
-
-            case 2:
-                if (!cell.neighbours[2].hasBeenChecked)
-                {
-                    cell.walls[2] = false;
-                    cell.surroundingWalls--;
-
-                    cell.neighbours[2].walls[0] = false;
-                    cell.neighbours[2].surroundingWalls--;
-                }
-                break;
-
-            case 3:
-                if (!cell.neighbours[3].hasBeenChecked)
-                {
-                    cell.walls[3] = false;
-                    cell.surroundingWalls--;
-
-                    cell.neighbours[3].walls[1] = false;
-                    cell.neighbours[3].surroundingWalls--;
-                }
-                break;
+            }
         }
 
         //for testing
@@ -181,13 +232,51 @@ public class MazeGenerator : MonoBehaviour
         maze[3, 1].surroundingWalls = 3;
         maze[3, 1].walls[3] = false;
         */
-
         return maze;
     }
 
-    private void WallRemover(Cell cell)
+    private void RemoveWallBetween(Cell cell, Cell neighbourCell)
     {
+        if (cell.heightPos < neighbourCell.heightPos)
+        {
+            cell.walls[1] = false;
+            cell.surroundingWalls--;
+            neighbourCell.walls[3] = false;
+            neighbourCell.surroundingWalls--;
+            return;
+        }
+        if (cell.heightPos > neighbourCell.heightPos)
+        {
+            cell.walls[3] = false;
+            cell.surroundingWalls--;
+            neighbourCell.walls[1] = false;
+            neighbourCell.surroundingWalls--;
+            return;
+        }
+        if (cell.widthPos < neighbourCell.widthPos)
+        {
+            cell.walls[2] = false;
+            cell.surroundingWalls--;
+            neighbourCell.walls[0] = false;
+            neighbourCell.surroundingWalls--;
+            return;
+        }
+        if (cell.widthPos > neighbourCell.widthPos)
+        {
+            cell.walls[0] = false;
+            cell.surroundingWalls--;
+            neighbourCell.walls[2] = false;
+            neighbourCell.surroundingWalls--;
+            return;
+        }
+    }
 
+    private void CheckCell(Cell cell)
+    {
+        do
+        {
+
+        } while (true);
     }
 
     private void SetNeighbours(Cell cellToCheck)
@@ -215,13 +304,6 @@ public class MazeGenerator : MonoBehaviour
         {
             cellToCheck.neighbours[2] = maze[h, w + 1];
         }
-
-        //check
-        for (int i = 0; i < 4; i++)
-        {
-            Debug.Log(cellToCheck.neighbours[i]);
-        }
-
     }
 
     //create maze full of walls
